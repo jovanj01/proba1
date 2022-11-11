436219b386a84b19aa4555899f8cc9de9f6f6c52
Jovan Jokic <jovanjokic555@gmail.com>
2022-11-11 14:39:41 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatakV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string broj = "";
+            Console.WriteLine("Unesi broj:");
+            broj = Console.ReadLine();
+
+            if (broj.Contains("3"))
+                Console.WriteLine("Ima 3");
+            else
+            {
+                Console.WriteLine("Nema 3!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
